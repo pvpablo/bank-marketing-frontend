86f22fe536ab23e5df8a2641678003484a386c50
Pablo Perez Villanueva <pablopv@google.com>
2020-06-20 08:48:37 -0500

    initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,90 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-row class="text-center">
+      <v-col class="mb-5 mt-10" cols="12">
+        <v-row justify="center">
+          <v-form ref="form">
+            <!-- <v-text-field v-model="name" :counter="10" label="Name" required></v-text-field> -->
+            <v-slider v-model="duration" :thumb-size="30" thumb-label="always" max="1000"></v-slider>
+            <!-- <v-select v-model="select" :items="items" :rules="[v => !!v || 'Item is required']"
+              label="Item" required></v-select> -->
+            <v-btn color="primary" class="mr-4" @click="predict" :disabled="waiting">Predict</v-btn>
+            <v-btn color="secondary" class="mr-4" @click="reset" :disabled="waiting">Reset Form</v-btn>
+          </v-form>
+        </v-row>
+      </v-col>
+    </v-row>
+    <v-row class="text-center" v-if="result">
+      <v-col class="mb-5" cols="12">
+        <v-row justify="center">
+          <h3>Latest Prediction</h3>
+        </v-row>
+        <v-row justify="center">
+          <p>Won't deposit confidence:  <span :class="class1">{{class1Confidence}}</span></p>
+        </v-row>
+        <v-row justify="center">
+        <p>Will deposit confidence: <span :class="class2">{{class2Confidence}}</span></p>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  export default {
+    data: () => ({
+      class1: '',
+      class2: '',
+      waiting: false,
+      class1Confidence: -1,
+      class2Confidence: -1,
+      result: false,
+      select: null,
+      items: [
+        'Item 1',
+        'Item 2',
+        'Item 3',
+        'Item 4',
+      ],
+      duration: 0,
+    }),
+    methods: {
+      reset () {
+        this.$refs.form.reset()
+        this.class1Confidence = -1
+        this.class2Confidence = -1
+        this.result=false
+      },
+      async predict () {
+        this.waiting=true
+        const BASE_URL = 'https://bank-marketing-endpoint-o2b4bu4ala-uc.a.run.app'
+        const response = await fetch(`${BASE_URL}?duration=${this.duration}`)
+        const data = await response.json();
+        this.class1Confidence = data[0].score
+        this.class2Confidence = data[1].score
+        if (this.class1Confidence >= 0.90) {
+          this.class1 = 'success'
+          this.class2 = 'error'
+        } else if (this.class1Confidence >= 0.60 && this.class1Confidence < 0.90) {
+          this.class1 = 'warning'
+          this.class2 = 'error'
+        } else if (this.class2Confidence >= 0.90) {
+          this.class1 = 'error'
+          this.class2 = 'success'
+        } else if (this.class2Confidence >= 0.60 && this.class2Confidence < 0.90) {
+          this.class1 = 'error'
+          this.class2 = 'warning'
+        } else {
+          this.class1 = 'error'
+          this.class2 = 'error'
+        }
+        this.result=true
+        this.waiting=false
+      }
+    },
   }
-}
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style>
+
 </style>
